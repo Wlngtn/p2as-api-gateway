27aaafe2ad3a00f361535f868e1484cd3faa8baa
Wellington <wellington.vasconcelos@gmail.com>
2022-07-26 09:52:33 -0300

    Alterações

---
+++ src/main/java/com/br/p2as/p2as/Api/Gateway/LoggingFilter.java
@@ -0,0 +1,25 @@
+package com.br.p2as.p2as.Api.Gateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class LoggingFilter implements GlobalFilter {
+
+	private Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
+	
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, 
+			GatewayFilterChain chain) {
+		logger.info("Path of the request received -> {}", 
+				exchange.getRequest().getPath());
+		return chain.filter(exchange);
+	}
+
+}
