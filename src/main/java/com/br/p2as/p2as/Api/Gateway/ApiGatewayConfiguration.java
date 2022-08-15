d5a1acf5ac4739b95a17dbd00b6215a3f477d817
Wellington <wellington.vasconcelos@gmail.com>
2022-08-15 19:00:51 -0300

    Entreg

--- src/main/java/com/br/p2as/p2as/Api/Gateway/ApiGatewayConfiguration.java
+++ src/main/java/com/br/p2as/p2as/Api/Gateway/ApiGatewayConfiguration.java
@@ -7,10 +7,29 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApiGatewayConfiguration {
+	private static String PREFIXO_GATEWAY = "/psas-api/";
+	private static String PREFIXO_URI_CLIENTE = "http://localhost:8080/";
+	private static String PREFIXO_URI_PROFISSIONAL = "http://localhost:8081/";
+	private static String PREFIXO_URI_PESSOA = "http://localhost:8082/";
+	private static String VERSAO = "v1/api/";
+	private static String CLIENTE = "clientes";
+	private static String PROFISSIONAIS = "profissionais";
+	private static String PESSOA = "pessoas";
 
 	@Bean
 	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
-		return builder.routes().build();
+			
+		return builder.routes()
+				.route(p -> p.path(PREFIXO_GATEWAY+VERSAO+PESSOA+"/**")
+						.uri(PREFIXO_URI_PESSOA+VERSAO+PESSOA))
+				
+				.route(p -> p.path(PREFIXO_GATEWAY+VERSAO+CLIENTE+"/**")
+						.uri(PREFIXO_URI_CLIENTE+VERSAO+CLIENTE))
+				
+				.route(p -> p.path(PREFIXO_GATEWAY+VERSAO+PROFISSIONAIS+"/**")
+						.uri(PREFIXO_URI_PROFISSIONAL+VERSAO+PROFISSIONAIS))
+
+				.build();
 	}
 	
 }
